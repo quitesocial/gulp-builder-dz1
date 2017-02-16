c56ebee05a84a05f28464d5f03b851e340fa48dd
quitesocial <quitesocial@yandex.ru>
2017-02-16 16:08:52 +0300

    edited dist/ sources

--- gulp/tasks/js.process.js
+++ gulp/tasks/js.process.js
@@ -4,7 +4,7 @@ module.exports = function() {
   $.gulp.task('js:process', function() {
     return $.gulp.src($.path.app)
       .pipe($.gp.sourcemaps.init())
-      .pipe($.gp.concat('app.js'))
+      .pipe($.gp.concat('main.js'))
       .pipe($.gp.sourcemaps.write())
       .pipe($.gulp.dest($.config.root + '/assets/js'))
   })
